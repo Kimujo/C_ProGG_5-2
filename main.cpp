0de3b64f6939356c86c16b7c7b0b3972178144d9
Kimujo <m.kamoliddinov@newuu.uz>
2025-09-24 12:45:21 +0500

    WÈÉK_5

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Problem_11
+int main() {
+    int d;
+    cout << " Waiting for your input integer to calculate the sum: ";
+    cin >> d;
+
+    int sum = 0;
+    int input= d;
+
+    while (input != 0) {
+        sum += input;
+        cin >> input;
+    }
+    cout << "The sum is: " << sum << endl;
+    return 0;
+}
+
+ Problem_12
+ int main() {
+    int n;
+    cout << " Let's see what you've got: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+Problem_13
+int main() {
+    float n;
+    cout << " Let's see what you've got again. This time in right triangle: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+Problem_14
+int main() {
+    int n;
+    cout << "This time in number triangle: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "You want to make a Triangle with 0. Good luck!";
+        ;
+    }
+
+    for ( int i=1; i<=n; i++) {
+        for (int j=1; j<=i; j++) {
+            cout << i;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+Problem_17
+int main() {
+    int d;
+    cout << " Waiting for your input integer to calculate the sum: ";
+    cin >> d;
+
+    int sum = 0;
+    int input= d;
+
+    while (input >  0) {
+        int digit = input % 10;
+        sum += digit;
+        input = input / 10;
+    }
+    cout << "The sum of your funny digits is : " << sum;
+
+    return 0;
+}
+
+Problem_22
+int main() {
+    float miles;
+    float kilometers;
+    cout << " Please enter the miles: ";
+    cin >> miles;
+
+    kilometers = miles * 1.609f;
+    cout << " Here is your kilometer conversion, my friend: " << kilometers << endl;
+    return 0;
+}
+
+Problem_18
+int main() {
+    int num;
+    int decimal = 0;
+
+    cout << "Enter a binary number, buddy: ";
+    cin >> num;
+
+    int base = 1;
+    int input = num;
+    while (input) {
+        int reminder = input % 10;
+        input = input / 10;
+        decimal += reminder * base;
+        base = base * 2;
+    }
+    cout << " Your decimal number is: " << decimal << "\n Enjoy!";
+
+
+
+    return 0;
+}*/
+
+
+int main() {
+    int n;
+    cout << "Let us find your harmonic series: ";
+    cin >> n;
+
+
+
+
+
+    return 0;
+}
+
+
+
